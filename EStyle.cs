696bffbf2604194e0aca9c174932569f31b04379
Kamil <mentos.96@o2.pl>
2023-06-18 15:19:23 +0200

    Write enum i class

---
+++ EStyle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage_shoes
+{
+    enum EStyle
+    {
+        tenisowki,
+        mokasyny,
+        sandaly,
+        klapki,
+        polbuty,
+        drewniaki,
+    }
+}
---
+++ Shoe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage_shoes
+{
+    class Shoe
+    {
+        public EStyle Style { get; private set; }
+
+        public string Color { get; private set; }
+
+        public Shoe(EStyle style, string color)
+        {
+            Style = style;
+            Color = color;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return $"{Color} {Style}";
+            }
+        }
+    }
+}
