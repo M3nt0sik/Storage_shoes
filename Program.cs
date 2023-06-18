94f52131ef9e5ff09368192a930232999c61563f
Kamil <mentos.96@o2.pl>
2023-06-18 16:07:42 +0200

    Write main funcionality

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
-{
+﻿using Storage_shoes;
+
+internal class Program
+{  
+    static ShoeCloset shoeCloset = new ShoeCloset();
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            shoeCloset.PrintShoes();
+            Console.WriteLine("\nWcisnij 'd' aby dodac, lub 'u', aby usunac buty: ");
+            char key = Console.ReadKey().KeyChar;
+
+            switch (key)
+            {
+                case 'd':
+                case 'D':
+                    shoeCloset.AddShoe();
+                    break;
+                case 'u':
+                case 'U':
+                    shoeCloset.RemoveShoe();
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 }
---
+++ ShoeCloset.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Storage_shoes
+{
+    class ShoeCloset
+    {
+        private readonly  List<Shoe> shoes = new List<Shoe>();
+        public void PrintShoes()
+        {
+            if(shoes.Count == 0) 
+            {
+                Console.WriteLine("\nSchowet na buty jest pusty.");
+            }
+            else
+            {
+                Console.WriteLine("\nSchowet na buty zawiera:");
+                int i = 1;
+                foreach (Shoe shoe in shoes)
+                {
+                    Console.WriteLine($"Para numer {i++}: {shoe.Description}");
+                }
+            }
+        }
+
+        public void AddShoe()
+        {
+            Console.WriteLine("\nDodaj parę butów.");
+            for(int i = 0; i<6; i++)
+            {
+                Console.WriteLine($"Wcisnij {i}: aby dodac {(EStyle)i}.");
+            }
+            Console.Write("Podaj fason: ");
+            if(int.TryParse(Console.ReadKey().KeyChar.ToString(), out int style))
+            {
+                Console.Write("\nPodaj kolor: ");
+                string color = Console.ReadLine();
+                Shoe shoe = new Shoe((EStyle)style, color);
+                shoes.Add(shoe);
+            }
+        }
+        public void RemoveShoe()
+        {
+            Console.WriteLine("\nWcisnij ktory numer chcesz usunac: ");
+            if( int.TryParse(Console.ReadKey().KeyChar.ToString(), out int shoeNumber) && (shoeNumber >=1) && (shoeNumber <= shoes.Count))
+            {
+                Console.WriteLine($"\nUsuwanie pary {shoes[shoeNumber - 1].Description}");
+                shoes.RemoveAt(shoeNumber - 1);
+            }
+        }
+    }
+}
